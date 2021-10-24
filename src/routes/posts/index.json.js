fde7faa949b54d62d653c2b9eccd01d7a26213e9
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-10-24 04:07:50 -0700

    connect to api and seput /posts.json route

---
+++ src/routes/posts/index.json.js
@@ -0,0 +1,33 @@
+import {client} from '$lib/graphql_client'
+import {gql} from 'graphql-request'
+
+export const get = async () => {
+  try {
+    const query = gql`
+      query Posts {
+        posts {
+          title
+          slug
+          date
+          excerpt
+          tags
+          coverImage{
+            url
+          }
+        }
+      }
+    `
+
+    const {posts} = await client.request(query)
+
+    return {
+      status: 200,
+      body: {posts}
+    }
+  } catch(error) {
+    return {
+      status: 500,
+      body: {error: 'Server error!'}
+    }
+  }
+}
