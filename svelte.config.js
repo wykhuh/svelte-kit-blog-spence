277874a805a96213b587debeb3744c000bbf402a
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-10-24 16:25:26 -0700

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-    }),
+    adapter: vercel()
   },
 
   preprocess: [
--- z_notes/2_deploy.md
+++ z_notes/2_deploy.md
@@ -3,6 +3,8 @@ https://kit.svelte.dev/docs#adapters
 
 ==
 npm i -D @sveltejs/adapter-static@next
-static
 
-npm i -D @sveltejs/adapter-static@next
+==
+
+npm i -g vercel
+npm i -D @sveltejs/adapter-vercel
